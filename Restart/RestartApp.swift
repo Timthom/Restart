91451f2e6535cde3505a8ae3d155240226c0607b
Timthom <monsson47thomas@gmail.com>
2023-04-03 18:33:22 +0200

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Thomas Månsson on 03/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
