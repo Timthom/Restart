e6e178789ac8e07e0087193cd4e4642278ebac42
Timthom <monsson47thomas@gmail.com>
2023-04-04 15:40:00 +0200

    Added animation

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,21 +12,133 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0
+    @State private var isAnimating: Bool = false
     
     
     //MARK: - BODY
     
     var body: some View {
-        VStack(spacing: 20) {
-            Text("OnboardingView")
-                .font(.largeTitle)
-            Button(action: {
-                //Some action
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }//: VSTACK
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all,edges: .all)
+            
+            VStack(spacing: 20) {
+              //MARK: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text("Share.")
+                        .font((.system(size: 60)))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    Text("""
+                        It´s not how much we give but how
+                        much love we put into giving.
+                        """)
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 10)
+                }//: HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -60)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+                
+                
+              //MARK: - CENTER
+                
+                ZStack	 {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        //.offset(x: isAnimating ? 0 : 200)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                    
+                }//: CENTER
+                
+                Spacer()
+                
+              //MARK: - FOOTER
+                
+                ZStack {
+                    // Parts of custom button
+                   
+                    // 1. BACKGROUND (STATIC)
+                    Capsule()
+                        .fill(Color(.white).opacity(0.2))
+                        Capsule()
+                        .fill(Color(.white).opacity(0.2))
+                        .padding(8)
+                    // 2. CALL_TO_ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80)
+                        
+                        Spacer()
+                    }
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight:  .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                    
+                                }
+                                .onEnded { _ in
+                                    withAnimation(Animation.easeOut(duration: 1)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                }
+                        )//: GESTURE
+                        Spacer()
+                    }//: HSTACKx§
+                }//: FOOTER
+                .frame(width: buttonWidth, height: 80, alignment: .center)
+                .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 60)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+            }//: VSTACK
+        }//: ZSTACK
+        .onAppear(perform: {
+            isAnimating = true
+        })
     }
 }
 
